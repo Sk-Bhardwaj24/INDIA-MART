e1953f4971892552172446615519b4664cb6437b
Saurabh Kumar Bhardwaj <me24.saurabhkumar@gmail.com>
2022-03-05 17:15:21 +0530

    adding country code in sign in page
    adding country code in sign in page

---
+++ components/main.js
@@ -0,0 +1,122 @@
+function changecode(i) {
+  if (i == 91) {
+    let councode = document.getElementById("modal2p");
+    let cc = document.getElementById("code1").src;
+    let cmg = document.getElementById("cmg");
+    councode.innerHTML = "+" + i;
+    cmg.src = cc;
+    document.querySelector("#countryflag").style.display = "none";
+  }
+  if (i == 1) {
+    let councode = document.getElementById("modal2p");
+    let cc = document.getElementById("code2").src;
+    let cmg = document.getElementById("cmg");
+    councode.innerHTML = "+" + i;
+    cmg.src = cc;
+    document.querySelector("#countryflag").style.display = "none";
+  }
+  if (i == 55) {
+    let councode = document.getElementById("modal2p");
+    let cc = document.getElementById("code3").src;
+    let cmg = document.getElementById("cmg");
+    councode.innerHTML = "+" + i;
+    cmg.src = cc;
+    document.querySelector("#countryflag").style.display = "none";
+  }
+  if (i == 86) {
+    let councode = document.getElementById("modal2p");
+    let cc = document.getElementById("code4").src;
+    let cmg = document.getElementById("cmg");
+    councode.innerHTML = "+" + i;
+    cmg.src = cc;
+    document.querySelector("#countryflag").style.display = "none";
+  }
+  if (i == 44) {
+    let councode = document.getElementById("modal2p");
+    let cc = document.getElementById("code5").src;
+    let cmg = document.getElementById("cmg");
+    councode.innerHTML = "+" + i;
+    cmg.src = cc;
+    document.querySelector("#countryflag").style.display = "none";
+  }
+  if (i == 33) {
+    let councode = document.getElementById("modal2p");
+    let cc = document.getElementById("code6").src;
+    let cmg = document.getElementById("cmg");
+    councode.innerHTML = "+" + i;
+    cmg.src = cc;
+    document.querySelector("#countryflag").style.display = "none";
+  }
+  if (i == 92) {
+    let councode = document.getElementById("modal2p");
+    let cc = document.getElementById("code7").src;
+    let cmg = document.getElementById("cmg");
+    councode.innerHTML = "+" + i;
+    cmg.src = cc;
+    document.querySelector("#countryflag").style.display = "none";
+  }
+  if (i == 7) {
+    let councode = document.getElementById("modal2p");
+    let cc = document.getElementById("code8").src;
+    let cmg = document.getElementById("cmg");
+    councode.innerHTML = "+" + i;
+    cmg.src = cc;
+    document.querySelector("#countryflag").style.display = "none";
+  }
+  if (i == 966) {
+    let councode = document.getElementById("modal2p");
+    let cc = document.getElementById("code9").src;
+    let cmg = document.getElementById("cmg");
+    councode.innerHTML = "+" + i;
+    cmg.src = cc;
+    document.querySelector("#countryflag").style.display = "none";
+  }
+  if (i == 82) {
+    let councode = document.getElementById("modal2p");
+    let cc = document.getElementById("code10").src;
+    let cmg = document.getElementById("cmg");
+    councode.innerHTML = "+" + i;
+    cmg.src = cc;
+    document.querySelector("#countryflag").style.display = "none";
+  }
+  if (i == 380) {
+    let councode = document.getElementById("modal2p");
+    let cc = document.getElementById("code11").src;
+    let cmg = document.getElementById("cmg");
+    councode.innerHTML = "+" + i;
+    cmg.src = cc;
+    document.querySelector("#countryflag").style.display = "none";
+  }
+  if (i == 81) {
+    let councode = document.getElementById("modal2p");
+    let cc = document.getElementById("code12").src;
+    let cmg = document.getElementById("cmg");
+    councode.innerHTML = "+" + i;
+    cmg.src = cc;
+    document.querySelector("#countryflag").style.display = "none";
+  }
+}
+function displaysign() {
+  document.querySelector(".bg-modal").style.display = "flex";
+}
+function closesign() {
+  document.querySelector(".bg-modal").style.display = "none";
+}
+function changecountrycode() {
+  document.querySelector("#countryflag").style.display = "flex";
+}
+
+// document.querySelector(".signopen").addEventListener("click", function () {
+//   document.querySelector(".bg-modal").style.display = "flex";
+// });
+// document.querySelector(".signopen1").addEventListener("click", function () {
+//   document.querySelector(".bg-modal").style.display = "flex";
+// });
+
+// document.querySelector(".close").addEventListener("click", function () {
+//   document.querySelector(".bg-modal").style.display = "none";
+// });
+// document.querySelector(".selector").addEventListener("click", function () {
+//   // document.querySelector(".bg-modal").style.display = "none";
+//   document.querySelector("#countryflag").style.display = "flex";
+// });
