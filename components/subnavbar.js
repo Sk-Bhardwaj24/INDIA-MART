7dc10682ef8d8ecacf89232d35c4982d47725928
Saurabh Kumar Bhardwaj <me24.saurabhkumar@gmail.com>
2022-03-07 20:14:04 +0530

    change nme

--- components/subnavbar.js
+++ components/subnavbar.js
@@ -22,8 +22,8 @@ const subHead = () => {
         <div id="h_sub2_left">
             <div><h3><a href="index.html">Home</a></h3></div>
             <div><h3><a href="page.html">Products & Services</a></h3></div>
-            <div><h3><a href="contactus.html">Contact Us</a></h3></div>
-            <div><h3>Contact Us</h3></div>
+            <div><h3><a >About us</a></h3></div>
+            <div><h3 href="contactus.html">Contact Us</h3></div>
         </div>
         <div><button class = 'h_sub2_btn'>Send Email</button></div>
     </div>
